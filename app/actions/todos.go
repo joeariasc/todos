e958f32ffcc8f3a4500acd5df7fa64a1dfe0915d
JArias <jarias@wawand.co>
2021-12-09 08:26:56 -0500

    retrieving all ToDos with the all method

--- app/actions/todos.go
+++ app/actions/todos.go
@@ -1,11 +1,27 @@
 package actions
 
 import (
+	"fmt"
 	"net/http"
+	"todos/app/models"
 
 	"github.com/gobuffalo/buffalo"
+	"github.com/gobuffalo/pop/v5"
 )
 
 func Index(c buffalo.Context) error {
+	tx := c.Value("tx").(*pop.Connection)
+	todos := models.Todos{}
+	err := tx.All(&todos)
+	if err != nil {
+		return err
+	}
+	todosType := fmt.Sprintf("%T", todos)
+	fmt.Println(todosType)
+	for _, todo := range todos {
+		fmt.Println("===========TODO")
+		fmt.Println(todo)
+	}
+
 	return c.Render(http.StatusOK, r.HTML("todos/index.plush.html"))
 }
