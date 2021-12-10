ca831891207c1d645085010f5e24f767312c2caa
JArias <jarias@wawand.co>
2021-12-10 09:06:20 -0500

    formatting code

--- app/models/todo.go
+++ app/models/todo.go
@@ -13,7 +13,7 @@ type Todo struct {
 	ID          uuid.UUID    `json:"id" db:"id"`
 	Title       string       `json:"title" db:"title"`
 	Details     nulls.String `json:"details" db:"details"`
-	LimitDate time.Time    `json:"limit_date" db:"limit_date"`
+	LimitDate   time.Time    `json:"limit_date" db:"limit_date"`
 	IsCompleted bool         `json:"is_completed" db:"is_completed"`
 	CreatedAt   time.Time    `json:"created_at" db:"created_at"`
 	UpdatedAt   time.Time    `json:"updated_at" db:"updated_at"`
@@ -24,7 +24,7 @@ func (t Todo) TableName() string {
 	return "todos"
 }
 
-// Todoes array model struct of Todo.
+// Todos array model struct of Todo.
 type Todos []Todo
 
 // String converts the struct into a string value.
