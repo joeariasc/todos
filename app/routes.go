0fdcaa7b1d84b5fe7fda8eb12605287103994058
JArias <jarias@wawand.co>
2022-01-12 14:51:42 -0500

    feat: adding Home route, skipping routes

--- app/routes.go
+++ app/routes.go
@@ -17,19 +17,22 @@ func setRoutes(root *buffalo.App) {
 	root.Use(middleware.Authorize)
 	root.Use(middleware.SetCurrentUser)
 
+	root.GET("/", actions.Home).Name("homeTodoPath")
+
 	root.GET("/users/new", actions.UsersNew)
 	root.POST("/users", actions.UsersCreate).Name("saveUserPath")
 	root.GET("/signin", actions.AuthNew)
 	root.POST("/signin", actions.AuthCreate).Name("saveAuthPath")
 	root.DELETE("/signout", actions.AuthDestroy)
 
-	root.GET("/", actions.Index).Name("listTodoPath")
+	root.GET("/todos", actions.Index).Name("listTodoPath")
 	root.GET("/todo/new", actions.NewTodo)
 	root.POST("/todo", actions.SaveTodo).Name("saveTodoPath")
 	root.GET("/todo/{todo_id}/edit", actions.EditTodo).Name("editTodoPath")
 	root.PUT("/todo/{todo_id}", actions.UpdateTodo).Name("updateTodoPath")
 	root.DELETE("/todo/{todo_id}/", actions.DeleteTodo).Name("deleteTodoPath")
 
-	root.Middleware.Skip(middleware.Authorize, actions.Index, actions.UsersNew, actions.UsersCreate, actions.AuthNew, actions.AuthCreate)
+	root.Middleware.Skip(middleware.Authorize, actions.Home, actions.UsersNew, actions.UsersCreate, actions.AuthNew, actions.AuthCreate)
+	root.Middleware.Skip(middleware.SetCurrentUser, actions.UsersNew, actions.UsersCreate, actions.AuthNew, actions.AuthCreate)
 	root.ServeFiles("/", base.Assets)
 }
