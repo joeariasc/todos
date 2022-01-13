d763ebc7fceefb38e999a6c604aa25b3108446a9
JArias <jarias@wawand.co>
2022-01-13 14:13:05 -0500

    fix: renaming test package, adding function to create user

--- app/models/models_test.go
+++ app/models/models_test.go
@@ -1,7 +1,8 @@
-package models
+package models_test
 
 import (
 	"testing"
+	"todos/app/models"
 
 	"github.com/gobuffalo/suite/v3"
 )
@@ -14,4 +15,20 @@ func Test_ModelSuite(t *testing.T) {
 	suite.Run(t, &ModelSuite{
 		Model: suite.NewModel(),
 	})
+}
+
+func (ms *ModelSuite) CreateUser() *models.User {
+	user := &models.User{
+		FirstName:            "Joe",
+		LastName:             "Arias",
+		Email:                "jarias@testing.com",
+		Password:             "joe123",
+		PasswordConfirmation: "joe123",
+	}
+	verrs, err := user.Create(ms.DB)
+
+	ms.NoError(err)
+	ms.False(verrs.HasAny())
+
+	return user
 }
