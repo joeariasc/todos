59a2775e65d934d73523575443cdb5417f90d740
JArias <jarias@wawand.co>
2022-01-13 14:13:53 -0500

    fix: renaming test package, more tweak

--- app/models/todo_test.go
+++ app/models/todo_test.go
@@ -1,22 +1,26 @@
-package models
+package models_test
 
 import (
 	"github.com/gobuffalo/nulls"
 	"time"
+	"todos/app/models"
 )
 
 func (ms *ModelSuite) Test_Todo_Create() {
+	u := ms.CreateUser()
+
 	count, err := ms.DB.Count("todos")
 	ms.NoError(err)
 	ms.Equal(0, count)
 
-	t := &Todo{
+	t := &models.Todo{
 		Title:       "ToDo Test",
 		Details:     nulls.NewString("It's really not much to add"),
 		LimitDate:   time.Now().AddDate(0, 0, 15),
 		IsCompleted: false,
 		CreatedAt:   time.Now(),
 		UpdatedAt:   time.Now(),
+		UserID:      u.ID,
 	}
 
 	err = ms.DB.Create(t)
